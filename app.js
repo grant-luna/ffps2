a95fbe6f62e660fc931cb76326f4321324a0475b
grant-luna <grantlunafidelity@gmail.com>
2024-02-09 20:21:55 -0800

    Create 'includes' subdirectory of 'views' directory

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 app.set('views', ['./views', './views/components']);
 app.set('view engine', 'pug');
 
+app.get('/', (request, response) => {
+  response.render('searches');
+});
+
 app.listen(3000, 'localhost', () => {
   console.log('Listening on Port 3000');
 });
